37d99d8e56bb022c2a8514098a7f2e6894d50b3f
Ilnur <ilnrzakirov@gmail.com>
2023-01-05 13:38:47 +0300

    Добавил jwt стратегию авторизации

--- auth/auth.py
+++ auth/auth.py
@@ -1,3 +1,12 @@
-from fastapi_users.authentication import CookieTransport
+from fastapi_users.authentication import (
+    CookieTransport,
+    JWTStrategy,
+)
+
+from config import config
 
 cookie_transport = CookieTransport(cookie_name="matcha", cookie_max_age=3600)
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=config.jwt.key, lifetime_seconds=3600)
