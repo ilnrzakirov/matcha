87e438baaf469ec7b4ad2b0cb732060ac0076ccc
Ilnur <ilnrzakirov@gmail.com>
2023-01-05 13:40:46 +0300

    Добавил бекенд авторизации

--- auth/auth.py
+++ auth/auth.py
@@ -1,4 +1,5 @@
 from fastapi_users.authentication import (
+    AuthenticationBackend,
     CookieTransport,
     JWTStrategy,
 )
@@ -10,3 +11,10 @@ cookie_transport = CookieTransport(cookie_name="matcha", cookie_max_age=3600)
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=config.jwt.key, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
